4b8dd8cac65c6f3c5f4c90b44110da5631307a41
stephenkati <stephenkatuli88@gmail.com>
2023-07-04 17:40:11 +0300

    add methods for genre, albums

--- app.rb
+++ app.rb
@@ -1,5 +1,7 @@
 require './data/preserve_data'
 require './classes/item'
+require './classes/genre'
+require './classes/music_album'
 
 class App
   attr_accessor :books, :labels, :items, :music_albums, :genres, :games, :authors
@@ -37,19 +39,44 @@ class App
   end
 
   def list_all_music_albums
-    puts 'music albums'
-  end
+    @music_albums = file_read('data/music_albums.json')
+    puts 'No music albums found' if @music_albums.empty?
 
-  def list_all_genres
-    puts 'genres'
-  end
+    @music_albums.each do |album|
+      puts "ID: #{album['id']}",
+           "Publish Date: #{album['publish_date']}, On Spotify: #{album['on_spotify']}"
+    end
 
-  def add_a_music_album
-    puts 'add music album'
+    puts '=========================='
   end
 
   def list_of_games
     puts 'games'
+  end
+
+  def list_all_genres
+    @genres = file_read('data/genres.json')
+    puts 'No Genres found' if @genres.empty?
+
+    @genres.each do |genre|
+      puts "ID: #{genre['id']}, Name: #{genre['name']}"
+    end
+
+    puts '=========================='
+  end
+
+  def add_a_music_album
+    puts 'Enter the albums publish date:'
+    publish_date = gets.chomp
+    puts 'Is the album on spotify? (Y/N)'
+    on_spotify = gets.chomp
+    on_spotify = true if %w[Y y].include?(on_spotify)
+    on_spotify = false if %w[N n].include?(on_spotify)
+    @music_albums << MusicAlbum.new(publish_date, on_spotify).to_hash
+    puts 'Music album added!'
+    file_write('./data/music_albums.json', @music_albums)
+
+    puts '=========================='
   end
 
   def list_all_authors
